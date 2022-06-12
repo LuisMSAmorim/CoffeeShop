409b7c9e53c41a3b5c884f36ae975f2a56719894
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-12 14:49:06 -0300

    Add StorageAccount local development connectionString && and cleaning DependencyInjectorHelper

--- CoffeeShop.Infrastructure.IoC/DependencyInjectorHelper.cs
+++ CoffeeShop.Infrastructure.IoC/DependencyInjectorHelper.cs
@@ -1,8 +1,10 @@
 ﻿using CoffeeShop.Domain.Model.Interfaces.Repositories;
 using CoffeeShop.Domain.Model.Interfaces.Services.Domain;
+using CoffeeShop.Domain.Model.Interfaces.Services.Infrastructure;
 using CoffeeShop.Domain.Services.Services;
 using CoffeeShop.Infrastructure.Data.Context;
 using CoffeeShop.Infrastructure.Data.Repositories;
+using CoffeeShop.Infrastructure.Services.Blob;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,10 +15,36 @@ public sealed class DependencyInjectorHelper
 {
     public static void Register(IServiceCollection services, IConfiguration configuration)
     {
+        DbContexts(services, configuration);
+
+        Repositories(services);
+
+        DomainServices(services);
+
+        InfrastructureServices(services, configuration);
+    }
+
+    private static void DbContexts(IServiceCollection services, IConfiguration configuration)
+    {
         services.AddDbContext<CoffeeShopDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection")));
+    }
 
+    private static void Repositories(IServiceCollection services)
+    {
         services.AddScoped<ICoffeRepository, CoffeeRepository>();
+    }
+
+    private static void DomainServices(IServiceCollection services)
+    {
         services.AddScoped<ICoffeeService, CoffeeService>();
+    }
+
+    private static void InfrastructureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var storageAccountConnectionString = configuration.GetConnectionString("StorageAccount");
+
+        services.AddScoped<IBlobService, BlobService>(provider => 
+            new BlobService(storageAccountConnectionString));
     }
 }
