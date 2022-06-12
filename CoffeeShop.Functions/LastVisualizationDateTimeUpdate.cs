bdb05c45197eff319928e48360524748b2728044
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-12 17:12:21 -0300

    Add LastVisualizationDateTimeUpdate function

---
+++ CoffeeShop.Functions/LastVisualizationDateTimeUpdate.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace CoffeeShop.Functions;
+
+public static class LastVisualizationDateTimeUpdate
+{
+    [FunctionName("LastVisualizationDateTime")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest request,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+        int coffeId = data?.Id;
+
+        var dbConnectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+
+        SqlConnection connection = new(dbConnectionString);
+
+        connection.Open();
+
+        var textSql = $@"UPDATE [dbo].[Coffee] SET [LastVisualization] = GETDATE() WHERE [Id] = {coffeId};";
+
+        SqlCommand cmd = new(textSql, connection);
+
+        var rowsAffected = cmd.ExecuteNonQuery();
+
+        log.LogInformation($"rowsAffected: {rowsAffected}");
+
+        return new OkResult();
+    }
+}
