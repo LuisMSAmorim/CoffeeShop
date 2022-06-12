2611fabb2eb8c4fdf3a44d3356fb661914e1cb95
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-12 11:27:34 -0300

    Add DbContext and DbContextFactory

---
+++ CoffeeShop.Infrastructure.Data/Context/CoffeeShopDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeShop.Domain.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Infrastructure.Data.Context;
+
+public class CoffeeShopDbContext : DbContext
+{
+    public CoffeeShopDbContext(DbContextOptions<CoffeeShopDbContext> options) : base(options) { }
+
+    public virtual DbSet<Coffee> Coffee { get; set; }
+}
---
+++ CoffeeShop.Infrastructure.Data/Context/CoffeeShopDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CoffeeShop.Infrastructure.Data.Context;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CoffeeShopDbContext>
+{
+    public CoffeeShopDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory()
+            + "/../CoffeeShop.WEB/appsettings.json").Build();
+
+        var builder = new DbContextOptionsBuilder<CoffeeShopDbContext>();
+        var connectionString = configuration.GetConnectionString("DatabaseConnection");
+
+        builder.UseSqlServer(connectionString);
+
+        return new CoffeeShopDbContext(builder.Options);
+    }
+}
