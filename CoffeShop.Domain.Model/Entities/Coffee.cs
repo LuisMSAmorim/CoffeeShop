32fb3f60f55e60bbc10c57bf15601c2bb38ecb89
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-12 14:07:02 -0300

    Add Data annotations to coffe Entity

--- CoffeShop.Domain.Model/Entities/Coffee.cs
+++ CoffeShop.Domain.Model/Entities/Coffee.cs
@@ -1,12 +1,21 @@
 ﻿using CoffeeShop.Domain.Model.Entities.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShop.Domain.Model.Entities;
 
 public sealed class Coffee : BaseEntity
 {
+    [Required]
+    [Display(Name = "Marca")]
     public string BrandName { get; set; }
+    [Required]
+    [Display(Name = "Nome do Produtor")]
     public string ProductorName { get; set; }
+    [Required]
+    [Display(Name = "Altitude de Plantio")]
     public int Altitude { get; set; }
+    [Required]
+    [Display(Name = "Localização de Plantio")]
     public string Location { get; set; }
     public string ImageUrl { get; set; }
 }
