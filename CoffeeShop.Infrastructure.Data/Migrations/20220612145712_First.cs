b1368a8b2548dffadfedabda51907f67585d4035
Luís Miguel Silva Amorim <83029922+LuisMSAmorim@users.noreply.github.com>
2022-06-12 11:57:38 -0300

    Create startup class and first db migration

---
+++ CoffeeShop.Infrastructure.Data/Migrations/20220612145712_First.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.Infrastructure.Data.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coffee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductorName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Altitude = table.Column<int>(type: "int", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coffee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coffee");
+        }
+    }
+}
---
+++ CoffeeShop.Web/Startup.cs
@@ -0,0 +1,23 @@
+﻿using CoffeeShop.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Web;
+
+public sealed class Startup
+{
+    public IConfigurationRoot Configuration { get; }
+
+    public Startup(IConfigurationRoot configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<CoffeeShopDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+
+    }
+}
